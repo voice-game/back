170ab11c57fa651bdc9b13f5389c5441007acded
kwon, minho <minhob38@gmail.com>
2021-04-29 06:13:44 +0900

    [MH] Update test case considering game name change(road roller -> little forest)

--- spec/gameController.spec.js
+++ spec/gameController.spec.js
@@ -1,38 +1,35 @@
-// test.js
-//const mochaStudy = require('./mochaStudy');
-const { expect } = require("chai");
 const request = require("supertest");
-// const { expect } = require("chai");
-const { server, db } = require("../server");
-const mongoose = require("mongoose")
-const generateUuid = require("uuid").v4
+const { expect } = require("chai");
+const mongoose = require("mongoose");
+const generateUuid = require("uuid").v4;
 const validateUuid = require("uuid-validate");
+const { server, db } = require("../server");
 
-describe('Game Controller Test ', function () {
+describe("Game Controller Test", () => {
   before((done) => {
     const checkDatabaseConnection = () => {
       if (db.readyState === 1) {
-        done()
+        done();
       } else {
-        done("db disconnection")
+        done("db disconnection");
       }
-    }
+    };
 
-    setTimeout(() => checkDatabaseConnection(), 1000)
-  })
+    setTimeout(() => checkDatabaseConnection(), 1000);
+  });
 
   describe("Monster Escape - fetch room database", () => {
     it("GET", (done) => {
       request(server)
-      .get("/api/games/monsterEscape")
-      .expect(200)
-      .expect("Content-Type", /json/)
-      .end((err, res) => {
-        if (err) { return done(err) };
-        expect(res.body.message).to.equal("success");
-        expect(res.body.data).to.be.instanceOf(Array);
-        done()
-      })
+        .get("/api/games/monsterEscape")
+        .expect(200)
+        .expect("Content-Type", /json/)
+        .end((err, res) => {
+          if (err) { return done(err) }
+          expect(res.body.message).to.equal("success");
+          expect(res.body.data).to.be.instanceOf(Array);
+          done();
+        });
     });
 
     it("POST - create room database", (done) => {
@@ -40,54 +37,54 @@ describe('Game Controller Test ', function () {
       const createdBy = mongoose.Types.ObjectId();
 
       request(server)
-      .post("/api/games/monsterEscape")
-      .set("Content-Type", "application/json")
-      .send({
-        gameTitle: "monsterEscape",
-        newRoomId,
-        createdBy,
-      })
-      .expect(201)
-      .expect("Content-Type", /json/)
-      .end((err, res) => {
-        if (err) { return done(err) };
+        .post("/api/games/monsterEscape")
+        .set("Content-Type", "application/json")
+        .send({
+          gameTitle: "monsterEscape",
+          newRoomId,
+          createdBy,
+        })
+        .expect(201)
+        .expect("Content-Type", /json/)
+        .end((err, res) => {
+          if (err) { return done(err) }
 
-        const { message, data } = res.body;
+          const { message, data } = res.body;
 
-        expect(message).to.equal("success");
-        expect(data).to.exist;
+          expect(message).to.equal("success");
+          expect(data).to.exist;
 
-        expect(data.title).to.exist;
-        expect(data.title).to.equal("monsterEscape");
+          expect(data.title).to.exist;
+          expect(data.title).to.equal("monsterEscape");
 
-        expect(data.roomId).to.exist;
-        expect(validateUuid(data.roomId)).to.equal(true);
-        expect(data.roomId).to.equal(newRoomId);
+          expect(data.roomId).to.exist;
+          expect(validateUuid(data.roomId)).to.equal(true);
+          expect(data.roomId).to.equal(newRoomId);
 
-        expect(data.createdBy).to.exist;
-        expect(mongoose.Types.ObjectId.isValid(data.createdBy)).to.equal(true);
-        expect(data.createdBy.toString()).to.equal(createdBy.toString());
+          expect(data.createdBy).to.exist;
+          expect(mongoose.Types.ObjectId.isValid(data.createdBy)).to.equal(true);
+          expect(data.createdBy.toString()).to.equal(createdBy.toString());
 
-        expect(data.players).to.exist;
-        expect(data.players[0].toString()).to.eql(createdBy.toString());
+          expect(data.players).to.exist;
+          expect(data.players[0].toString()).to.eql(createdBy.toString());
 
-        done()
-      })
+          done();
+        });
     });
   });
 
-  describe("Road Roller - fetch room database", () => {
+  describe("Little Forest - fetch room database", () => {
     it("GET", (done) => {
       request(server)
-      .get("/api/games/roadRoller")
-      .expect(200)
-      .expect("Content-Type", /json/)
-      .end((err, res) => {
-        if (err) { return done(err) };
-        expect(res.body.message).to.equal("success");
-        expect(res.body.data).to.be.instanceOf(Array);
-        done()
-      })
+        .get("/api/games/littleForest")
+        .expect(200)
+        .expect("Content-Type", /json/)
+        .end((err, res) => {
+          if (err) { return done(err) }
+          expect(res.body.message).to.equal("success");
+          expect(res.body.data).to.be.instanceOf(Array);
+          done();
+        });
     });
 
     it("POST - create room database", (done) => {
@@ -95,54 +92,54 @@ describe('Game Controller Test ', function () {
       const createdBy = mongoose.Types.ObjectId();
 
       request(server)
-      .post("/api/games/roadRoller")
-      .set("Content-Type", "application/json")
-      .send({
-        gameTitle: "roadRoller",
-        newRoomId,
-        createdBy,
-      })
-      .expect(201)
-      .expect("Content-Type", /json/)
-      .end((err, res) => {
-        if (err) { return done(err) };
+        .post("/api/games/littleForest")
+        .set("Content-Type", "application/json")
+        .send({
+          gameTitle: "littleForest",
+          newRoomId,
+          createdBy,
+        })
+        .expect(201)
+        .expect("Content-Type", /json/)
+        .end((err, res) => {
+          if (err) { return done(err) }
 
-        const { message, data } = res.body;
+          const { message, data } = res.body;
 
-        expect(message).to.equal("success");
-        expect(data).to.exist;
+          expect(message).to.equal("success");
+          expect(data).to.exist;
 
-        expect(data.title).to.exist;
-        expect(data.title).to.equal("roadRoller");
+          expect(data.title).to.exist;
+          expect(data.title).to.equal("littleForest");
 
-        expect(data.roomId).to.exist;
-        expect(validateUuid(data.roomId)).to.equal(true);
-        expect(data.roomId).to.equal(newRoomId);
+          expect(data.roomId).to.exist;
+          expect(validateUuid(data.roomId)).to.equal(true);
+          expect(data.roomId).to.equal(newRoomId);
 
-        expect(data.createdBy).to.exist;
-        expect(mongoose.Types.ObjectId.isValid(data.createdBy)).to.equal(true);
-        expect(data.createdBy.toString()).to.equal(createdBy.toString());
+          expect(data.createdBy).to.exist;
+          expect(mongoose.Types.ObjectId.isValid(data.createdBy)).to.equal(true);
+          expect(data.createdBy.toString()).to.equal(createdBy.toString());
 
-        expect(data.players).to.exist;
-        expect(data.players[0].toString()).to.eql(createdBy.toString());
+          expect(data.players).to.exist;
+          expect(data.players[0].toString()).to.eql(createdBy.toString());
 
-        done()
-      })
+          done();
+        });
     });
   });
 
   describe("Energy Battle - fetch room database", () => {
     it("GET", (done) => {
       request(server)
-      .get("/api/games/energyBattle")
-      .expect(200)
-      .expect("Content-Type", /json/)
-      .end((err, res) => {
-        if (err) { return done(err) };
-        expect(res.body.message).to.equal("success");
-        expect(res.body.data).to.be.instanceOf(Array);
-        done()
-      })
+        .get("/api/games/energyBattle")
+        .expect(200)
+        .expect("Content-Type", /json/)
+        .end((err, res) => {
+          if (err) { return done(err) }
+          expect(res.body.message).to.equal("success");
+          expect(res.body.data).to.be.instanceOf(Array);
+          done();
+        });
     });
 
     it("POST - create room database", (done) => {
@@ -150,39 +147,39 @@ describe('Game Controller Test ', function () {
       const createdBy = mongoose.Types.ObjectId();
 
       request(server)
-      .post("/api/games/energyBattle")
-      .set("Content-Type", "application/json")
-      .send({
-        gameTitle: "energyBattle",
-        newRoomId,
-        createdBy,
-      })
-      .expect(201)
-      .expect("Content-Type", /json/)
-      .end((err, res) => {
-        if (err) { return done(err) };
+        .post("/api/games/energyBattle")
+        .set("Content-Type", "application/json")
+        .send({
+          gameTitle: "energyBattle",
+          newRoomId,
+          createdBy,
+        })
+        .expect(201)
+        .expect("Content-Type", /json/)
+        .end((err, res) => {
+          if (err) { return done(err) }
 
-        const { message, data } = res.body;
+          const { message, data } = res.body;
 
-        expect(message).to.equal("success");
-        expect(data).to.exist;
+          expect(message).to.equal("success");
+          expect(data).to.exist;
 
-        expect(data.title).to.exist;
-        expect(data.title).to.equal("energyBattle");
+          expect(data.title).to.exist;
+          expect(data.title).to.equal("energyBattle");
 
-        expect(data.roomId).to.exist;
-        expect(validateUuid(data.roomId)).to.equal(true);
-        expect(data.roomId).to.equal(newRoomId);
+          expect(data.roomId).to.exist;
+          expect(validateUuid(data.roomId)).to.equal(true);
+          expect(data.roomId).to.equal(newRoomId);
 
-        expect(data.createdBy).to.exist;
-        expect(mongoose.Types.ObjectId.isValid(data.createdBy)).to.equal(true);
-        expect(data.createdBy.toString()).to.equal(createdBy.toString());
+          expect(data.createdBy).to.exist;
+          expect(mongoose.Types.ObjectId.isValid(data.createdBy)).to.equal(true);
+          expect(data.createdBy.toString()).to.equal(createdBy.toString());
 
-        expect(data.players).to.exist;
-        expect(data.players[0].toString()).to.eql(createdBy.toString());
+          expect(data.players).to.exist;
+          expect(data.players[0].toString()).to.eql(createdBy.toString());
 
-        done()
-      })
+          done();
+        });
     });
   });
 });
